1e05b96a2f5ff52e9437819382eade7fda5a62d7
Giorgio Rindonone <giorgio.rindonone@gmail.com>
2020-06-26 17:37:26 +0200

    jsnack v0.5

--- js/jsnack6.js
+++ js/jsnack6.js
@@ -1,18 +1,19 @@
-var result = documet.getElementById("result6");
+
 
 var btn6 = document.getElementById("bottone6");
+var result = document.getElementById("result6");
 
 btn6.addEventListener("click", function () {
 var nome1 = prompt("inserisci il primo nome");
 var nome2 = prompt("inserisci il secondo nome");
 
 if (nome1.length > nome2.length) {
-  result.innerHTML = ("il nome più lungo è" nome1);
-  result.innerHTML = ("il nome più corto è" nome2);
+  result.innerHTML = ("il nome più lungo è" + nome1);
+  result.innerHTML = ("il nome più corto è" + nome2);
 
 } else if (nome2.length > nome1.length) {
-  result.innerHTML = ("il nome più lungo è" nome2);
-  result.innerHTML = ("il nome più corto è" nome1);
+  result.innerHTML = ("il nome più lungo è" + nome2);
+  result.innerHTML = ("il nome più corto è" + nome1);
 } else (nome2.length == nome1.length) {
   result.innerHTML = ("sono lunghi uguali");
 }
